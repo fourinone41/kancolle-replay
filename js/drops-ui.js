a380e6edaed494a1f4f50b4136355f51fb08a3ea
fourinone41 <pz200411@gmail.com>
2021-05-18 09:44:01 -0400

    drop tables: Spring21 E-1, E-2

--- js/drops-ui.js
+++ js/drops-ui.js
@@ -548,6 +548,7 @@ var NOTES_SPECIAL = {
 	'48-1_normal': 'Note: Shimushu, Kunashiri, Etorofu\'s replacements are unclear.',
 	'48-1_boss': 'Note: Shimushu, Kunashiri, Etorofu\'s replacements are unclear but include Jintsuu and Naka.',
 	'48-5_boss': 'Note: Almost no data for B rank, assumed same pattern as other boss nodes from the event.',
+	'50-2_normal': 'Note: Slots 1 to 6 unclear',
 };
 
 // })();
